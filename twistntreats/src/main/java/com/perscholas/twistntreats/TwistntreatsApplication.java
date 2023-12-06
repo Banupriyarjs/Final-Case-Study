354e7f77e6a639e218f53edc4f3228d892f0cbc3
banupriyarjs <banupriya.bubloo@gmail.com>
2023-12-05 23:48:08 -0500

    Initial commit

---
+++ twistntreats/src/main/java/com/perscholas/twistntreats/TwistntreatsApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.twistntreats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwistntreatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwistntreatsApplication.class, args);
+	}
+
+}
---
+++ twistntreats/src/test/java/com/perscholas/twistntreats/TwistntreatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.twistntreats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwistntreatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
