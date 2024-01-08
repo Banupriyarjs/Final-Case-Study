e1f129b7bc0f1e950da8d9f2b83e6de66dc4a9a4
banupriyarjs <banupriya.bubloo@gmail.com>
2024-01-08 04:12:37 -0500

    Added @PreAuthorize annotation  to check for admin rights @PreAuthorize("hasAuthority('ADMIN')") to access Category Controller

--- twistntreats/src/main/java/com/perscholas/twistntreats/controller/CategoryController.java
+++ twistntreats/src/main/java/com/perscholas/twistntreats/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.perscholas.twistntreats.security.AuthenticatedUserService;
 import com.perscholas.twistntreats.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
+@PreAuthorize("hasAuthority('ADMIN')")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
